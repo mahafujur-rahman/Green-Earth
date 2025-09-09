21f891de54f11ecfa5a16d72e547716d610d9c0a
MD. Mahafujur Rahman <mdmahafujurrahman2040@gmail.com>
2025-09-09 11:52:11 +0600

    categories functionality uploaded

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,17 @@
 // load all plants
-function loadAllPlants(){
+function loadAllPlants() {
     const url = `https://openapi.programming-hero.com/api/plants`;
     fetch(url)
-    .then((response) => response.json())
-    .then((data) => showAllPlants(data.plants));
+        .then((response) => response.json())
+        .then((data) => showAllPlants(data.plants));
 }
-loadAllPlants()
 
 // show all plants
-function showAllPlants(plants){
+function showAllPlants(plants) {
     const container = document.getElementById("plants-container");
     container.innerHTML = "";
 
-    for(const plant of plants){
+    for (const plant of plants) {
         const div = document.createElement("div");
         div.innerHTML = `
         
@@ -20,7 +19,7 @@ function showAllPlants(plants){
                 <img class="w-full h-[200px] object-cover object-center rounded-lg" src="${plant.image}" alt="">
 
                 <h4 class="text-xl font-semibold">${plant.name}</h4>
-                <p class="text-base font-light">${plant.description}</p>
+                <p class="text-base font-light line-clamp-2">${plant.description}</p>
                 <div class="flex justify-between flex-wrap">
                     <div class="badge badge-soft badge-success">${plant.category}</div>
                     <p class="font-semibold">৳${plant.price}</p>
@@ -31,5 +30,36 @@ function showAllPlants(plants){
         `;
         container.appendChild(div);
     }
-    
 }
+
+// load all categories
+function loadAllCategories() {
+    const url = `https://openapi.programming-hero.com/api/categories`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => showAllCategories(data.categories));
+}
+
+//show all categories
+function showAllCategories(categories) {
+    const categoryContainer = document.getElementById("categories-container");
+    categoryContainer.innerHTML = "";
+
+    categories.forEach((category) => {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+        
+            <button class="btn btn-wide btn-sm rounded bg-transparent border-none text-black justify-start hover:bg-green-500 hover:text-white active:bg-green-600 mb-4 md:px-5">
+                ${category.category_name}
+            </button>
+        
+        `;
+
+        categoryContainer.appendChild(div);
+    });
+}
+
+loadAllCategories();
+loadAllPlants();
